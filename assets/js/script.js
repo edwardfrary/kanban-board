0a92ee3befe9498cf33649b9c70d62b53f772c51
edwardfrary <edwardfrary@gmail.com>
2021-05-01 20:21:39 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 var taskSelector = "";
 var buttonEl = document.querySelector("#save-task");
 var listEl = document.querySelector("#list");
-var addItemEl = document.createElement("li");
+
 
 buttonEl.addEventListener("click", function () {
     var taskSelector = prompt("What is the task?");
+    var addItemEl = document.createElement("li");
     addItemEl.textContent = taskSelector;
     addItemEl.className = "task-item";
     listEl.appendChild(addItemEl);
-    console.log = (taskSelector);
 });
 
